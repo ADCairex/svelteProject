534e01fcbcbb5cb4d7f611b9cf1c8256b105fe50
ADCairex <angelmartinezguardiola@gmail.com>
2021-10-27 20:25:51 +0200

    create form to add new alumn

--- src/App.svelte
+++ src/App.svelte
@@ -8,12 +8,13 @@
 	}
 
 	const onAlumnSubmitHandler = (e) => {
-
+		console.log(alumn);
 	}
 </script>
 
 <main>
-	<div class="alumnsContainer">
+	<div class="addAlumnsContainer">
+		<h2>Agregar nuevo usuario:</h2>
 		<form on:submit|preventDefault={onAlumnSubmitHandler}>
 			<label for="alumnName">Nombre del alumno:</label>
 			<input 
@@ -22,6 +23,14 @@
 				id="alumnName"
 				placeholder="Nombre"
 			/>
+			<label for="alumnCourse">Curso al que pertenece el alumno</label>
+			<input
+				type="text"
+				bind:value={alumn.course}
+				id="alumnCourse"
+				placeholder="Curso"
+			/>
+			<button>Agregar</button>
 		</form>
 	</div>
 </main>
