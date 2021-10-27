01ac25b6d6dff551a9033aefc6494b50d4768992
ADCairex <angelmartinezguardiola@gmail.com>
2021-10-27 20:15:38 +0200

    create form

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,30 @@
 <script>
+
+	let alumn = {
+		name: '',
+		course: '',
+		subject: '',
+		idWork: '',
+	}
+
+	const onAlumnSubmitHandler = (e) => {
+
+	}
 </script>
 
 <main>
+	<div class="alumnsContainer">
+		<form on:submit|preventDefault={onAlumnSubmitHandler}>
+			<label for="alumnName">Nombre del alumno:</label>
+			<input 
+				bind:value={alumn.name}
+				type="text"
+				id="alumnName"
+				placeholder="Nombre"
+			/>
+		</form>
+	</div>
 </main>
+
 <style>
 </style>
